6487953a749ef06066c2c47470b9e6f53a6d66aa
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-07-20 00:48:39 +0330

    create Custom User Model in accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUserModel(AbstractUser):
+    GENDER_CHOICES = (
+        ("Male", "Male"),
+        ("Female", "Female")
+    )
+    age = models.PositiveIntegerField(black=True, null=True)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
