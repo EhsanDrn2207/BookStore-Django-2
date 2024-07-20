d751cf7925902edb39baa7348c78c38f913a2628
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-07-20 12:23:59 +0330

    add home page test and signup form test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+    def test_home_page_url(self):
+        response = self.client.get("")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_template_used(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home_page.html")
+
+    def test_home_page_component(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "Home Page")
+
+
+class SignUpPageTest(TestCase):
+    def test_signup_page_url(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_page_name(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_page_template_used(self):
+        response = self.client.get(reverse("signup"))
+        self.assertTemplateUsed(response, "registration/signup_page.html")
+
+    def test_signup_page_form(self):
+        username = "new_username"
+        email = "new_email"
+        user = get_user_model().objects.create_user(username=username, email=email)
+        self.assertEqual(get_user_model().objects.count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].email, email)
+        self.assertEqual(get_user_model().objects.all()[0].username, username)
