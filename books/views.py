1d11f3e6dad04fcd2e2b45d412b72334ad8faa08
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-07-21 14:46:13 +0330

    implement book detail page

--- books/views.py
+++ books/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
 from .models import Book
 
 
 def books_list_view(request):
     books = Book.objects.all()
     return render(request, "books/books_list.html", context={'books': books})
+
+
+def book_detail_view(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, "books/book_detail.html", context={'book': book})
